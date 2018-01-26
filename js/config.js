dea305e90b349c14a3c82e175d0f8e6083511d1c
James Wang <j.wang5441@gmail.com>
2018-01-25 17:39:37 -0800

    removes config file

--- js/config.js
+++
@@ -1,4 +0,0 @@
-var apiKey = "a8da787bf432963c5cad6f17800755b0";
-var apiToken = "d80ca8b08b485e1bae9526bc519de716059e3960a8a5e692ddcd577564f48d0b";
-var apiUrl = "https://api.trello.com/1";
-var boardId = "5a6a62753448dddc79c88876";
